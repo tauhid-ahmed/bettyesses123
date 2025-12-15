7aa6f1f8939359e005a7a7fc5c526e001d224b60
Tauhid <tauhidahmed.dev@gmail.com>
2025-12-15 17:28:02 +0600

    Refactor Container component to accept size prop and improve className management

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,3 +1,19 @@
-export default function Container({ children }: React.PropsWithChildren) {
-  return <div className="max-w-332 w-full mx-auto px-8">{children}</div>;
+import { cn } from "@/lib/utils";
+
+type Props = {
+  children: React.ReactNode;
+  size: "sm" | "lg";
+};
+
+export default function Container({ children, size = "lg" }: Props) {
+  return (
+    <div
+      className={cn("w-full mx-auto px-8", {
+        "max-w-332": size === "lg",
+        "max-w-196.5": size === "sm",
+      })}
+    >
+      {children}
+    </div>
+  );
 }
