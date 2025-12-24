ee36d27add2a9afc9308b68081c40a10fe140d71
Tauhid Ahmed <tauhidxtauhid@gmail.com>
2025-12-24 08:20:08 +0600

    refactor: replace h2 with Heading component in BooksList for consistency

--- app/(dashboard)/dashboard/books/_components/BookList.tsx
+++ app/(dashboard)/dashboard/books/_components/BookList.tsx
@@ -5,15 +5,16 @@ import Link from "next/link";
 import { booksIdPath } from "@/paths";
 import AdminBookCard from "@/features/books/components/AdminBookCard";
 import { Plus } from "lucide-react";
+import Heading from "@/components/Heading";
 
 export default function BooksList() {
   return (
     <Section padding="none">
       <div className="@container">
-        <div className="flex items-center justify-between gap-4 flex-wrap">
-          <h2 className="text-dark-800 font-medium text-[32px] mb-4">
+        <div className="flex items-center justify-between gap-4 flex-wrap mb-4">
+          <Heading as="h2" size="h3">
             Customize & Upload New Books
-          </h2>
+          </Heading>
           <Button asChild>
             <Link href="/dashboard/books/create-book">
               <Plus /> Add New Books
