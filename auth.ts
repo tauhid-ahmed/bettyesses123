2c87d58586acef09669d29a6441c5cd8b4509b7a
Tauhid Ahmed <tauhidxtauhid@gmail.com>
2025-12-31 11:02:04 +0600

    add initial not found page

--- auth.ts
+++ auth.ts
@@ -158,6 +158,7 @@ export const authConfig: NextAuthConfig = {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
+  trustHost: true,
   secret: process.env.NEXTAUTH_SECRET,
 };
 
