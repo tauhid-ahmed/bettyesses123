032e608729a16e999abb9174ef35b42a950559f2
Tauhid Ahmed <tauhidxtauhid@gmail.com>
2026-01-08 14:35:34 +0600

    fix protected route

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,18 @@
 import Container from "@/components/Container";
 import AuthContainer from "@/features/auth/components/AuthContainer";
 import { Suspense } from "react";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function AuthLayout({ children }: React.PropsWithChildren) {
+export default async function AuthLayout({
+  children,
+}: React.PropsWithChildren) {
+  const session = await auth();
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <div className="bg-gray-50 min-h-screen">
       <Container>
