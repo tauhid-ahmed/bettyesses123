ff647fb0d0abc043635bf5fbbdce3c343b996b44
alok4d <alokroy602701@gmail.com>
2026-01-09 16:38:01 +0600

    get successfull all promo codes in admin dashboard

---
+++ features/admin/promo-codes/types/index.ts
@@ -0,0 +1,19 @@
+export type PromoCode = {
+  id: string;
+  code: string;
+  startTime: string;
+  endTime: string;
+  minOrderAmount: number;
+  discountPercentage: number;
+  perPersonUseLimit: number;
+  isActive: boolean;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type PromoCodeResponse = {
+  success: boolean;
+  statusCode: number;
+  message: string;
+  data: PromoCode[];
+};
