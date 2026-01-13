eb11cf992a3f4e1e9e55d44c1badf74457fc24f0
Fardus Hassan <webdev.fardus@gmail.com>
2026-01-13 15:14:33 +0600

    intregate payment

--- constants/index.ts
+++ constants/index.ts
@@ -2,4 +2,3 @@ export const BACKEND_API_URL = process.env.BACKEND_API_URL;
 
 // Stripe Keys
 export const STRIPE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
-export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
